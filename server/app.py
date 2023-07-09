f763635a0a3d73bb3afa72294c23ceb6560b1745
Ritwik Math <ritwikmath@gmail.com>
2023-07-09 10:14:04 +0530

    absolute env

--- server/app.py
+++ server/app.py
@@ -9,5 +9,9 @@ app: Flask = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.get('/env')
+def env():
+    return os.getenv('MONGO_URL')
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
